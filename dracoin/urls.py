817ed7ed68626b190a3e24ed46ae751fac8f3a0a
Pavlo DOMOZHYRSKYI <pdomozhy@e1r6p13.unit.ua>
2018-12-17 12:47:12 +0200

    second

---
+++ dracoin/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'dracoin'
+urlpatterns = [
+	path('', views.IndexView.as_view(), name='index'),
+	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('', views.index, name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:article_id>/', views.detail, name='detail'),
+    # path('<int:tag_id>/tag/', views.results, name='results'),
+    # path('<int:image_id>/img/', views.vote, name='vote'),
+]
+
+
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
--- dracoin/views.py
+++ dracoin/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Article, Tag, Image
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def index(request):
+#     latest_article_list = Article.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.title for q in latest_article_list])
+#     return HttpResponse(output)
+
+# def index(request):
+#     latest_article_list = Article.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('dracoin/index.html')
+#     context = {
+#         'latest_article_list': latest_article_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+    template_name = 'dracoin/index.html'
+    context_object_name = 'latest_article_list'
+
+    def get_queryset(self):
+        return Article.objects.order_by('-pub_date')[:5]
+    # def get_queryset(self):
+    #     """
+    #     Return the last five published questions (not including those set to be
+    #     published in the future).
+    #     """
+    #     return Question.objects.filter(
+    #         pub_date__lte=timezone.now()
+    #     ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Article
+    template_name = 'dracoin/detail.html'
+
