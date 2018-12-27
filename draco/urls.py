fa8154f7dad1376825583a27ebb655914abe1cd2
Pavlo DOMOZHYRSKYI <pdomozhy@e1r6p13.unit.ua>
2018-12-27 12:36:09 +0200

    add static files, add redirect to main page, add admin-panel

--- draco/urls.py
+++ draco/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
+from .views import redirect_blog
+
 urlpatterns = [
+	path('', redirect_blog),
 	path('dracoin/', include('dracoin.urls')),
 	path('admin/', admin.site.urls),
 ]
